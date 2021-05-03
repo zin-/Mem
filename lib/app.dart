b6e4108ea342258e57805f64cf5006e4f008bbe0
zin- <noyu718@gmail.com>
2021-05-03 21:15:15 +0900

    ロケールを設定

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:mem/mem_detail.dart';
+
+const locale = Locale("ja", "JP");
 
 class App extends StatelessWidget {
   @override
@@ -10,6 +13,15 @@ class App extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MemDetail(),
+      locale: locale,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        locale,
+      ],
     );
   }
 }
